b3f1cd836e39df66d41ef3aa92b315eaec23f495
Aaron Hoskins <aaron.hoskins@mobileappscompany.us>
2018-12-14 18:10:55 -0500

    Initial commit

---
+++ app/src/main/java/com/mobileapps/mvvm/viewmodels/LoginViewModel.java
@@ -0,0 +1,51 @@
+package com.mobileapps.mvvm.viewmodels;
+
+import android.databinding.BaseObservable;
+import android.databinding.Bindable;
+
+
+import com.mobileapps.mvvm.BR;
+import com.mobileapps.mvvm.models.User;
+
+public class LoginViewModel extends BaseObservable {
+    private User user;
+
+
+    private String successMessage = "Login was successful";
+    private String errorMessage = "Email or Password not valid";
+
+    @Bindable
+    public String toastMessage = null;
+
+
+    public String getToastMessage() {
+        return toastMessage;
+    }
+
+
+    private void setToastMessage(String toastMessage) {
+
+        this.toastMessage = toastMessage;
+        notifyPropertyChanged(BR.toastMessage);
+    }
+
+    public LoginViewModel() {
+        user = new User("", "");
+    }
+
+    public void afterEmailTextChanged(CharSequence s) {
+        user.setEmail(s.toString());
+    }
+
+    public void afterPasswordTextChanged(CharSequence s) {
+        user.setPassword(s.toString());
+    }
+
+    public void onLoginClicked() {
+        setToastMessage(user.isInputDataValid() ? successMessage : errorMessage);
+//        if (user.isInputDataValid())
+//            setToastMessage(successMessage);
+//        else
+//            setToastMessage(errorMessage);
+    }
+}
